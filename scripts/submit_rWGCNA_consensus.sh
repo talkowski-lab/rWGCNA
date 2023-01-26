376be17c07ba6dac07a3df07756cd0707eed49ce
hwick <33556230+hwick@users.noreply.github.com>
2023-01-26 18:52:17 -0500

    Update submit_rWGCNA_consensus.sh

--- scripts/submit_rWGCNA_consensus.sh
+++ scripts/submit_rWGCNA_consensus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ###############################################
+# This script is used to submit rWGCNA_consensus.R to the server
+# by Heather Wick hwick@broad institute.org
 # usage:
 # bash submit_rWGCNA_args_blockwise.sh project_name [num_iter] /path/to/Data_counts_combat_norm_log2.txt /path/to/output/base/dir/ /path/to/traitData.txt [block_size] [bool_use_default_soft_power_threshold]
 # example:
@@ -17,7 +19,7 @@ iter=$2 #number of times to resample/bootstrap data set
 data=$3 #log2 normalized expression data
 baseDir=$4 #base directory to contain the output
 traitData=$5 #trait data with Samples column corresponding to colnames of data
-maxBlockSize=$6 #5000 default; I recommend using a number slightly larger than the number of genes, if you have the computational power
+maxBlockSize=$6 #I recommend using a number slightly larger than the number of genes, if you have the computational power
 defaultPower=$7 #should be T or F; F will calculate soft power threshold for each resampled data set and choose the median
 WORKDIR=${name}_NumIter${iter}
 PROJECTDIR=${baseDir}${WORKDIR}
